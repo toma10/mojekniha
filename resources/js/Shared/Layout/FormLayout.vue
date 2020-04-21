afe0ae76dad39b21027cab950e5ec3a949cff782
Tomáš Máčala <tomas.macala@seznam.cz>
2020-04-21 08:55:20 +0200

    Remove text-center

--- resources/js/Shared/Layout/FormLayout.vue
+++ resources/js/Shared/Layout/FormLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md text-center">
       <slot name="header" />
 
       <div :class="{'mt-4': $page.flash}">
