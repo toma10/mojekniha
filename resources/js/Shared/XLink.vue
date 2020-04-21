e253c2b82390a52608c540b42776dfd0a512564a
Tomáš Máčala <tomas.macala@seznam.cz>
2020-04-21 08:54:53 +0200

    Use inertia-link

--- resources/js/Shared/XLink.vue
+++ resources/js/Shared/XLink.vue
@@ -1,10 +1,10 @@
 <template>
-  <a
+  <inertia-link
     :href="href"
     class="font-medium text-indigo-600 text-sm leading-5 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
   >
     <slot />
-  </a>
+  </inertia-link>
 </template>
 
 <script>
