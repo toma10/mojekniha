73b5c3ff90c6012f538832e7d4c9ab34cbb38c58
Tomáš Máčala <tomas.macala@seznam.cz>
2020-04-28 10:42:20 +0200

    Simplify FCN

--- tests/Unit/Domain/User/Models/UserTest.php
+++ tests/Unit/Domain/User/Models/UserTest.php
@@ -13,7 +13,7 @@ class UserTest extends TestCase
     /** @test */
     public function it_can_find_user_by_email()
     {
-        $user = factory(\App\Domain\User\Models\User::class)->create(['email' => 'johndoe@example.com']);
+        $user = factory(User::class)->create(['email' => 'johndoe@example.com']);
 
         $foundUser = User::findByEmail('johndoe@example.com');
 
@@ -23,7 +23,7 @@ class UserTest extends TestCase
     /** @test */
     public function it_returns_avatar_url()
     {
-        $user = factory(\App\Domain\User\Models\User::class)->make(['email' => 'johndoe@example.com']);
+        $user = factory(User::class)->make(['email' => 'johndoe@example.com']);
         $emailMd5 = md5('johndoe@example.com');
 
         $this->assertEquals("https://www.gravatar.com/avatar/${emailMd5}?d=mp&s=150", $user->avatarUrl(150));
@@ -34,8 +34,8 @@ class UserTest extends TestCase
     /** @test */
     public function it_can_determine_if_is_admin()
     {
-        $user = factory(\App\Domain\User\Models\User::class)->create(['is_admin' => false]);
-        $admin = factory(\App\Domain\User\Models\User::class)->create(['is_admin' => true]);
+        $user = factory(User::class)->create(['is_admin' => false]);
+        $admin = factory(User::class)->create(['is_admin' => true]);
 
         $this->assertFalse($user->isAdmin());
         $this->assertTrue($admin->isAdmin());
@@ -44,8 +44,8 @@ class UserTest extends TestCase
     /** @test */
     public function it_can_determine_if_has_verified_email()
     {
-        $unverifiedUser = factory(\App\Domain\User\Models\User::class)->create(['email_verified_at' => null]);
-        $verifiedUser = factory(\App\Domain\User\Models\User::class)->create(['email_verified_at' => now()]);
+        $unverifiedUser = factory(User::class)->create(['email_verified_at' => null]);
+        $verifiedUser = factory(User::class)->create(['email_verified_at' => now()]);
 
         $this->assertFalse($unverifiedUser->hasVerifiedEmail());
         $this->assertFalse($unverifiedUser->is_verified);
