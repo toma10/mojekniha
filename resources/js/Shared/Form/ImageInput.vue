9ae80c1c02e0f55f333fc34ab539dc600c5e5ccb
Tomáš Máčala <tomas.macala@seznam.cz>
2020-04-30 11:53:48 +0200

    Preview image

--- resources/js/Shared/Form/ImageInput.vue
+++ resources/js/Shared/Form/ImageInput.vue
@@ -25,19 +25,28 @@
           @keyup.space="click"
         >
           <div class="text-center">
-            <svg
-              class="mx-auto h-12 w-12 text-gray-400"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <div class="mx-auto h-12 w-12">
+              <svg
+                v-if="!file"
+                class="w-full h-full text-gray-400"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 48 48"
+              >
+                <path
+                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <img
+                v-else
+                ref="preview"
+                class="w-full h-full object-cover rounded"
+                alt="Preview"
+              >
+            </div>
             <div class="mt-1 text-sm text-gray-600">
               <span v-if="!file">Upload a file or drag and drop</span>
               <div v-else>
@@ -126,6 +135,15 @@ export default {
     change(files) {
       if (files.length) {
         this.file = files[0]
+
+        const reader = new FileReader()
+
+        reader.onload = (event) => {
+          this.$refs.preview.src = event.target.result
+        }
+
+        reader.readAsDataURL(this.file)
+
         this.$emit('input', this.file)
       } else {
         this.clear()
