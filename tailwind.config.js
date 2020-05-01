28640ec05cd276213a5e6823b10fef005213c909
Tomáš Máčala <tomas.macala@seznam.cz>
2020-05-01 15:04:03 +0200

    Use purgecss in tailwind v1.4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  purge: [
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.vue',
+    './resources/js/**/*.js',
+  ],
   theme: {
     extend: {
       fontFamily: {
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,9 @@
 const mix = require('laravel-mix')
-const WebpackShellPlugin = require('webpack-shell-plugin');
+const WebpackShellPlugin = require('webpack-shell-plugin')
 const path = require('path')
 const tailwindcss = require('tailwindcss')
 const cssImport = require('postcss-import')
 const cssNesting = require('postcss-nesting')
-const purgecss = require('@fullhuman/postcss-purgecss')
 
 mix.js('resources/js/app.js', 'public/js')
   .postCss('resources/css/app.css', 'public/css/app.css')
@@ -13,13 +12,6 @@ mix.js('resources/js/app.js', 'public/js')
       cssImport(),
       cssNesting(),
       tailwindcss('tailwind.config.js'),
-      ...mix.inProduction() ? [
-        purgecss({
-          content: ['./resources/views/**/*.blade.php', './resources/js/**/*.vue'],
-          defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
-          whitelistPatternsChildren: [/nprogress/],
-        }),
-      ] : [],
     ],
   })
   .webpackConfig({
